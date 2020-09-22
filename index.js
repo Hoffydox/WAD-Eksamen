a872c90939698b06e8cf7ff59731e9c9e94331e7
Andreas Hoffmann <Hoffmann@Andreass-MacBook-Pro-3.local>
2020-09-22 12:03:31 +0200

    JSON resHeader

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const login = require('./routes/login');
 const users = require('./routes/users');
-// const setJSON = require('./middleware/setResponseHeaderToJSON');
+const setJSON = require('./middleware/setResponseHeaderToJSON');
 
 const app = express();
 const myPort = 8171;
 
 app.use(cors());
-// app.use(setJSON);
+app.use(setJSON);
 app.use(express.json()); // --> req.body
 //
 
