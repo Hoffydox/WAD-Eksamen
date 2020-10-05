fc5f299d9697cd7909f1214dc4a38d0feda2b4e9
Andreas Hoffmann <Hoffmann@Andreass-MacBook-Pro-3.local>
2020-10-05 17:47:17 +0200

    changes

--- front-end/projekter.js
+++ front-end/projekter.js
@@ -4,23 +4,23 @@ const serverURL = 'http://127.0.0.1:8171';
 //const serverURL = "https://mmd.ucn.dk:8171";
 
 
-    console.log("Script loaded");
-    getData();
+console.log("Script loaded");
+getData();
 
 
 
 // GET projekter til index.html
 function getData() {
     const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             const data = JSON.parse(xhttp.responseText);
             console.log(data);
             console.log(data[2].projectName);
 
             // fører koden videre til en function ved navn renderprojects
             renderProjects(data);
-            
+
         }
     }
     // xhttp.open('GET', `${WPurl}posts/?tags=${postInfoId}`, true);
@@ -32,9 +32,9 @@ function getData() {
 
 function renderProjects(data) { // starter Render funktionen. Alt bliver renderet i querySelector elementet.
     document.querySelector('#testrenderer').innerHTML = `
+    <section class="projekter">
     <h2>${data[0].projectName}</h2>
     <button class="btn">
-    <img src="#" alt="">
             <a href="projekt.html">Se projekt</a>
         </button>
     <h2>${data[1].projectName}</h2>
@@ -44,10 +44,10 @@ function renderProjects(data) { // starter Render funktionen. Alt bliver rendere
     <br>
     <h2>${data[2].projectName}</h2>
     <button  class="btn">
-            <a href="projekt.html">Se projekt</a>
-            
+            <a href="projekt.html">Se projekt</a> 
         </button>
       
+       
       `;
     // Math.round() Runder tallet op.
     // <img src="/front-end/images/${data.projects[0].icon}">
